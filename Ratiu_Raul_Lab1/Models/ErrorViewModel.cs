513b6f3317b85550fef5b0ef2ace00be0897811b
Raul <Raul@RAUL>
2023-10-19 17:57:22 +0300

    Add project files.

---
+++ Ratiu_Raul_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ratiu_Raul_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
