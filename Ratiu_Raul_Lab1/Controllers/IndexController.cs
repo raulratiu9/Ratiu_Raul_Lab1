3da69facba9a646941ae83eb3d95fbfc3d2abeeb
raulratiu9 <raulratiu2@gmail.com>
2023-10-19 18:43:14 +0300

    feat: insert 3 new methods in IndexController

---
+++ Ratiu_Raul_Lab1/Controllers/IndexController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace Ratiu_Raul_Lab1.Controllers
+{
+    public class IndexController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome to our website!";
+        }
+        public string GreetUser()
+        {
+            return "Hello!";
+        }
+        public string SelfIntroduction(string name = "Raul", int age = 21)
+        {
+            return $"My name is {name} and I'm {age} years old";
+        }
+    }
+}
